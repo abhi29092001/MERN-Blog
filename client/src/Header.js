89498d8a271d19c88e84e90f22b2cb4c45f8f0db
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2024-01-20 19:27:31 +0530

    first commit

---
+++ client/src/Header.js
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "./UserContext";
+
+export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
+      <nav>
+        {username && (
+          <>
+            <Link
+              to="/create"
+              style={{
+                fontWeight: "bold",
+                color: "rgb(93, 223, 93)",
+                fontSize: "1.2rem",
+              }}
+            >
+              Create new post
+            </Link>
+            <a
+              onClick={logout}
+              style={{
+                fontWeight: "bold",
+                color: "rgb(93, 223, 93)",
+                fontSize: "1.2rem",
+              }}
+            >
+              Logout ({username})
+            </a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
